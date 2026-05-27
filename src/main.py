91f4b8bfb1af91bf94cd56e80ae64f30f9f77b02
Denys Shevchenko <denshevchenko19@gmail.com>
2026-05-27 16:21:21 +0200

    add initial project structure with .gitignore, pre-commit configuration, and basic files

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from src.paths import PROJECT_ROOT
+
+print(f"{PROJECT_ROOT=}")
---
+++ src/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATA_ROOT = PROJECT_ROOT / "data"
+SRC_ROOT = PROJECT_ROOT / "src"
+
+DATA_ROOT.mkdir(exist_ok=True, parents=True)
